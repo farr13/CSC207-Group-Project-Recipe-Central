6aa610a0643e34acd9bff4d25749afa69d2d6550
Afra-A-A <afraazad25@gmail.com>
2023-10-30 21:02:49 -0400

    added todo for infgredient entity

--- src/users/entity/Ingredient.java
+++ src/users/entity/Ingredient.java
@@ -1,4 +1,11 @@
 package users.entity;
 
+/*
+    TODO: Implement me by wednesday! Note:
+     -  String for ingredient
+     -  Double for quantity
+     -  String for quantity measurement
+     -  getters and setters
+ */
 public class Ingredient {
 }
