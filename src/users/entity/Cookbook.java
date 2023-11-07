a9688e24cd2ac2da7f5accab2cb37de69fe4102f
farr13 <farrakhan.abdoulaye@mail.utoronto.ca>
2023-11-06 23:53:55 -0500

    Added Cookbook Entity

--- src/users/entity/Cookbook.java
+++ src/users/entity/Cookbook.java
@@ -1,4 +1,62 @@
 package users.entity;
 
-public class Cookbook {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Cookbook implements Iterable<Recipe>{
+
+    private final String name;
+
+    private ArrayList<Recipe> Recipes = new ArrayList<Recipe>();
+
+    Cookbook(String name, ArrayList<Recipe> Recipes){
+        this.name = name;
+        this.Recipes = Recipes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Recipe> getRecipes() {
+        return Recipes;
+    }
+
+
+    public void addRecipes(Recipe recipe) {
+        this.Recipes.add(recipe);
+    }
+
+    public void removeRecipes(Recipe recipe) {
+        if (!this.Recipes.contains(recipe)){
+            throw new NoSuchElementException();
+        }
+        else {
+          this.Recipes.remove(recipe);
+        }
+    }
+
+    @Override
+    public Iterator<Recipe> iterator() {
+        return new CookbookItr();
+    }
+
+    private class CookbookItr implements Iterator<Recipe>{
+        private int currindex;
+        @Override
+        public boolean hasNext() {
+            return currindex < Recipes.size();
+        }
+
+        @Override
+        public Recipe next() {
+           if (!hasNext()) {
+               throw new NoSuchElementException();
+           }
+           Recipe currRecipe = Recipes.get(currindex);
+           currindex += 1;
+           return currRecipe;
+        }
+    }
 }
