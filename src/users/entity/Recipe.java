3bae8a8ad3d9f3f06b1a2a453a7ec69684f71f51
zachfu9669 <zachfu9669@gmail.com>
2023-11-07 10:43:01 -0500

    implemented recipeEntity

--- src/users/entity/Recipe.java
+++ src/users/entity/Recipe.java
@@ -1,4 +1,38 @@
-package users.entity;
+import users.entity.Ingredient;
 
-public class Recipe {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Recipe implements Iterable<Ingredient> {
+    private List<Ingredient> ingredients;
+    private String name;
+    private List<String> instructions;
+
+    public Recipe(String name) {
+        this.name = name;
+        ingredients = new ArrayList<>();
+        instructions = new ArrayList<>();
+    }
+
+    public List<Ingredient> getIngredientList() {
+        return ingredients;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    public void addInstruction(String instruction) {
+        instructions.add(instruction);
+    }
+
+    @Override
+    public Iterator<Ingredient> iterator() {
+        return ingredients.iterator();
+    }
+
+    public Instruction getInstructions() {
+        return () -> instructions.iterator();
+    }
 }
