3c62a5d0d93672f7ea8a0b1b6e26d9e53da76757
Afra-A-A <afraazad25@gmail.com>
2023-11-10 15:06:40 -0500

    updated search input data with array list

--- src/users/service/search_recipes/SearchInputData.java
+++ src/users/service/search_recipes/SearchInputData.java
@@ -1,20 +1,22 @@
 package users.service.search_recipes;
 
-public class SearchInputData {
-    private String ingredients, mealType, diet, health;
+import java.util.ArrayList;
 
-    public SearchInputData(String ingredients, String mealType, String diet, String health){
+public class SearchInputData {
+    private ArrayList<String> ingredients, mealType, diet, health;
+
+    public SearchInputData(ArrayList<String> ingredients, ArrayList<String> mealType, ArrayList<String> diet, ArrayList<String> health){
         this.ingredients = ingredients;
         this.mealType = mealType;
         this.diet = diet;
         this.health = health;
     }
 
-    public String getIngredients() { return ingredients; }
+    public ArrayList getIngredients() { return ingredients; }
 
-    public String getMealType() { return mealType; }
+    public ArrayList getMealType() { return mealType; }
 
-    public String getDiet() { return diet; }
+    public ArrayList getDiet() { return diet; }
 
-    public String getHealth() { return health; }
+    public ArrayList getHealth() { return health; }
 }
