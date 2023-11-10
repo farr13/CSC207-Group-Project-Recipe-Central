115adac1092b7d9b4bae6d168b31337c7704db72
Afra-A-A <afraazad25@gmail.com>
2023-11-10 13:46:25 -0500

    created SearchInputData.java

--- src/users/service/search_recipes/SearchInputData.java
+++ src/users/service/search_recipes/SearchInputData.java
@@ -1,5 +1,20 @@
 package users.service.search_recipes;
 
 public class SearchInputData {
+    private String ingredients, mealType, diet, health;
 
+    public SearchInputData(String ingredients, String mealType, String diet, String health){
+        this.ingredients = ingredients;
+        this.mealType = mealType;
+        this.diet = diet;
+        this.health = health;
+    }
+
+    public String getIngredients() { return ingredients; }
+
+    public String getMealType() { return mealType; }
+
+    public String getDiet() { return diet; }
+
+    public String getHealth() { return health; }
 }
