212bb0e7e2e1d2275721e771ef26e0fae5c60bc3
Afra-A-A <afraazad25@gmail.com>
2023-11-11 12:37:48 -0500

    Created execute method

--- src/users/service/search_recipes/SearchInteractor.java
+++ src/users/service/search_recipes/SearchInteractor.java
@@ -1,2 +1,27 @@
-package users.service.search_recipes;public class SearchInteractor {
+package users.service.search_recipes;
+
+public class SearchInteractor implements SearchInputBoundary {
+    private APICaller apiCaller;
+    private InputDataURLConverter inputDataConverter;
+    private JsonOutputDataConverter outputDataConverter;
+    private SearchPresenter searchPresenter;
+
+    public SearchInteractor(APICaller apiCaller, InputDataURLConverter inputDataConverter, JsonOutputDataConverter outputDataConverter, SearchPresenter searchPresenter){
+        this.apiCaller = apiCaller;
+        this.inputDataConverter = inputDataConverter;
+        this.outputDataConverter = outputDataConverter;
+        this.searchPresenter = searchPresenter;
+    }
+
+    @Override
+    public void execute(SearchInputData searchInputData) {
+        // Convert inputdata data to a tuple with strings
+        String requestURLComponents = inputDataConverter.convertToURL(searchInputData);
+        // Call the API Caller which gives a string in Json format
+        String responceStr = apiCaller.execute(requestURLComponents);
+        // Convert Json String to Recipe output data
+        OutputData outputData = outputDataConverter.convertToRecipies(responceStr);
+        // Call the presenter
+        searchPresenter.prepareSuccessView(outputData);
+    }
 }
