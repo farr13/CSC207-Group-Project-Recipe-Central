14df8c35f54893d4f1bf97ec16435a11ed5795e5
Afra-A-A <afraazad25@gmail.com>
2023-11-13 20:17:48 -0500

    created the class with convertToURL method.

--- src/users/service/search_recipes/EdamamURLGenerator.java
+++ src/users/service/search_recipes/EdamamURLGenerator.java
@@ -1,2 +1,32 @@
-package users.service.search_recipes;public class EdamamURLGenerator {
+package users.service.search_recipes;
+
+public class EdamamURLGenerator implements InputDataURLConverter {
+    String apiID, apiKey;
+
+    public EdamamURLGenerator(String id, String key){
+        apiID = id;
+        apiKey = key;
+    }
+
+    @Override
+    public String convertToURL(SearchInputData searchInputData) {
+        String ingredients = "", mealTypes = "", diets = "", healths = "", requestURL = "";
+        for (String ingredient: searchInputData.getIngredients()){
+            ingredients = ingredient + "%2C%20";
+        }
+        if (ingredients.length() > 1)
+            ingredients = ingredients.substring(0, ingredients.length() - 6);
+
+        for (String mealType: searchInputData.getMealType())
+            mealTypes = "&mealType=" + mealType;
+
+        for (String diet: searchInputData.getDiet())
+            diets = "&diet=" + diet;
+
+        for (String health: searchInputData.getHealth())
+            healths = "&health=" + health;
+
+        return "https://api.edamam.com/api/recipes/v2?type=public&q=" + ingredients + "&app_id=" + apiID +
+                "&app_key=" + apiKey + diets + healths + mealTypes;
+    }
 }
