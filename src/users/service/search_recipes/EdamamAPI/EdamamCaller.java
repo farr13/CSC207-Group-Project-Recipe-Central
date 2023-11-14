026012dce84751ee99195bca27ef595a1be8f7d4
Afra-A-A <afraazad25@gmail.com>
2023-11-13 20:37:54 -0500

    created EdamamCaller class and made an execute method

---
+++ src/users/service/search_recipes/EdamamAPI/EdamamCaller.java
@@ -0,0 +1,35 @@
+package users.service.search_recipes.EdamamAPI;
+
+import users.service.search_recipes.API_Interface.APICaller;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class EdamamCaller implements APICaller {
+    @Override
+    public String execute(String pullRequestURL) {
+        try {
+            URL url = new URL(pullRequestURL);
+
+            // Open the connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // Get the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder responseContent = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                responseContent.append(inputLine);
+            }
+            in.close();
+
+            return responseContent.toString();
+
+        } catch (Exception e) {
+            return "no results";
+        }
+    }
+}
