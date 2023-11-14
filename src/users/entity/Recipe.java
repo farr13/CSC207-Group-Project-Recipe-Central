20325362ad82b5e654cee7c6f1fe19e5e1382358
Zachary Fu <zacharyfu@Zacharys-MacBook-Pro.local>
2023-11-13 19:30:33 -0500

    update Recipe to view, rename, and delete

--- src/users/entity/Recipe.java
+++ src/users/entity/Recipe.java
@@ -1,26 +1,71 @@
 package users.entity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Recipe {
     private String name;
     private List<Ingredient> ingredients;
-    private String instructionsURL; // Instructions as url link
+    private String instructionsURL; // Instructions as a URL link
 
     public Recipe(String name, String instructionsURL) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (instructionsURL == null || instructionsURL.trim().isEmpty()) {
+            throw new IllegalArgumentException("Instructions URL cannot be null or empty");
+        }
+
         this.name = name;
-        ingredients = new ArrayList<>();
         this.instructionsURL = instructionsURL;
         this.ingredients = new ArrayList<>();
     }
 
     // Get the recipe name
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
     // Get a copy of the ingredient list to prevent modification
-    public List<Ingredient> getIngredients() { return ingredients; }
+    public List<Ingredient> getIngredients() {
+        return Collections.unmodifiableList(ingredients);
+    }
 
-    public String getInstructions() { return instructionsURL; }
+    public String getInstructionsURL() {
+        return instructionsURL;
+    }
 
+    // Add a single ingredient to the recipe
+    public void addIngredient(Ingredient ingredient) {
+        if (ingredient == null) {
+            throw new IllegalArgumentException("Ingredient cannot be null");
+        }
+        ingredients.add(ingredient);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipe recipe = (Recipe) o;
+        return name.equals(recipe.name) &&
+                Objects.equals(instructionsURL, recipe.instructionsURL) &&
+                Objects.equals(ingredients, recipe.ingredients);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, instructionsURL, ingredients);
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                ", instructionsURL='" + instructionsURL + '\'' +
+                '}';
+    }
 }
