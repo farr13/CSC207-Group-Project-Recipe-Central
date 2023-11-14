038389c63fdb90b3a4a9af92081892530c51a70a
Afra-A-A <afraazad25@gmail.com>
2023-11-13 20:56:39 -0500

    created convertRecipes method in JsonOutputDataConverter interface

--- src/users/service/search_recipes/API_Interface/JsonOutputDataConverter.java
+++ src/users/service/search_recipes/API_Interface/JsonOutputDataConverter.java
@@ -1,5 +1,7 @@
 package users.service.search_recipes.API_Interface;
 
-public interface JsonOutputDataConverter {
+import users.service.search_recipes.DataTypes.SearchOutputData;
 
+public interface JsonOutputDataConverter {
+    public SearchOutputData convertRecipes(String requestResponse);
 }
