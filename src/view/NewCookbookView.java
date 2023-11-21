66c4dc032da245daa67cd8b806d0367843e01ba5
Aditya G <aditya.gautam@mail.utoronto.ca>
2023-11-20 20:31:46 -0500

    Baseline view model classes

---
+++ src/view/NewCookbookView.java
@@ -0,0 +1,20 @@
+package view;
+
+import app.NotImplementedException;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class NewCookbookView extends JPanel implements ActionListener, PropertyChangeListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new NotImplementedException();
+    }
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        throw new NotImplementedException();
+    }
+}
