44655d8529802158abed8f7791a7b54bbd142e8d
farr13 <farrakhan.abdoulaye@mail.utoronto.ca>
2023-11-21 23:09:52 -0500

    Created existByTitle, save and get

--- src/users/service/make_cookbook/MakeCookbookDataAccessInterface.java
+++ src/users/service/make_cookbook/MakeCookbookDataAccessInterface.java
@@ -1,9 +1,13 @@
 package users.service.make_cookbook;
 
-import users.entity.Recipe;
-
-import java.util.ArrayList;
+import users.entity.Cookbook;
 
 public interface MakeCookbookDataAccessInterface {
+    /*Check if the cookbook title already exist*/
+    boolean existByTitle(String identifier);
+    /*saves the cookbook*/
+    void save(Cookbook cookbook);
+    /*gets the cookbook based on title*/
+    Cookbook get(String title);
 
 }
