13007583a740e69b6a94b5d8c1a3170b6d0c24aa
farr13 <farrakhan.abdoulaye@mail.utoronto.ca>
2023-11-22 09:13:16 -0500

    Created deletecookbook_usecase structure

---
+++ src/users/service/delete_cookbook/DeleteCookbookDataAccessInterface.java
@@ -0,0 +1,4 @@
+package users.service.delete_cookbook;
+
+public interface DeleteCookbookDataAccessInterface {
+}
---
+++ src/users/service/delete_cookbook/DeleteCookbookOutputData.java
@@ -0,0 +1,11 @@
+package users.service.delete_cookbook;
+
+public class DeleteCookbookOutputData {
+
+    private final String[] storedCookbooks;
+
+    public DeleteCookbookOutputData(String[] storedCookbooks) {
+        this.storedCookbooks = storedCookbooks;
+    }
+
+}
