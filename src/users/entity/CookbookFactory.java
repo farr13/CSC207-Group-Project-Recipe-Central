0282d750e07dea65a08d31551d7409a9c61807ca
Jalal <jalal.mustafayev@mail.utoronto.ca>
2023-11-25 16:54:47 -0500

    Implemented Basic Factory

--- src/users/entity/CookbookFactory.java
+++ src/users/entity/CookbookFactory.java
@@ -1,4 +1,14 @@
 package users.entity;
 
+import java.util.ArrayList;
+
 public class CookbookFactory {
+
+    public static Cookbook createCookbook(String name, ArrayList<Recipe> recipes) {
+        return new Cookbook(name, recipes);
+    }
+
+    public static Cookbook createEmptyCookbook(String name) {
+        return new Cookbook(name, new ArrayList<>());
+    }
 }
--- src/users/entity/RecipeFactory.java
+++ src/users/entity/RecipeFactory.java
@@ -1,4 +1,16 @@
 package users.entity;
 
+import java.util.List;
+
 public class RecipeFactory {
+
+    public static Recipe createRecipe(String name, String instructionsURL, List<Ingredient> ingredients) {
+        Recipe recipe = new Recipe(name, instructionsURL);
+        recipe.getIngredients().addAll(ingredients);
+        return recipe;
+    }
+
+    public static Recipe createEmptyRecipe(String name, String instructionsURL) {
+        return new Recipe(name, instructionsURL);
+    }
 }
