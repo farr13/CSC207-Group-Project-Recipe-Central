f207444f710583c31d613383a8483edde812a765
Jalal <jalal.mustafayev@mail.utoronto.ca>
2023-11-26 13:26:39 -0500

    Added extra functions to the Factories

--- src/users/entity/CookbookFactory.java
+++ src/users/entity/CookbookFactory.java
@@ -11,4 +11,30 @@ public class CookbookFactory {
     public static Cookbook createEmptyCookbook(String name) {
         return new Cookbook(name, new ArrayList<>());
     }
+
+    public static Cookbook createCookbookWithDefaultRecipes(String name) {
+        // Create a cookbook with some default recipes
+        ArrayList<Recipe> defaultRecipes = new ArrayList<>();
+        defaultRecipes.add(new Recipe("Default Pasta", "Instructions for default pasta"));
+        defaultRecipes.add(new Recipe("Default Cake", "Instructions for default pasta"));
+        return new Cookbook(name, defaultRecipes);
+    }
+
+    public static Cookbook createRandomizedCookbook(String name, int numRecipes) {
+        // Create a cookbook with a specified number of randomly generated recipes
+        ArrayList<Recipe> randomRecipes = generateRandomRecipes(numRecipes);
+        return new Cookbook(name, randomRecipes);
+    }
+
+    private static ArrayList<Recipe> generateRandomRecipes(int numRecipes) {
+        // Logic to generate random recipes (replace with your implementation)
+        ArrayList<Recipe> randomRecipes = new ArrayList<>();
+        for (int i = 1; i <= numRecipes; i++) {
+            String recipeName = "Random Recipe " + i;
+            String instructions = "Instructions for " + recipeName;
+            randomRecipes.add(new Recipe(recipeName, instructions));
+        }
+        return randomRecipes;
+    }
+
 }
--- src/users/entity/RecipeFactory.java
+++ src/users/entity/RecipeFactory.java
@@ -1,16 +1,24 @@
 package users.entity;
 
+import users.entity.Ingredient;
+import users.entity.Recipe;
+
 import java.util.List;
 
 public class RecipeFactory {
 
     public static Recipe createRecipe(String name, String instructionsURL, List<Ingredient> ingredients) {
+        validateParameters(name, instructionsURL, ingredients);
+
         Recipe recipe = new Recipe(name, instructionsURL);
         recipe.getIngredients().addAll(ingredients);
+
         return recipe;
     }
 
-    public static Recipe createEmptyRecipe(String name, String instructionsURL) {
-        return new Recipe(name, instructionsURL);
+    private static void validateParameters(String name, String instructionsURL, List<Ingredient> ingredients) {
+        if (name == null || name.isEmpty() || instructionsURL == null || instructionsURL.isEmpty() || ingredients == null || ingredients.isEmpty()) {
+            throw new IllegalArgumentException("Name, instructions URL, and ingredients are mandatory parameters.");
+        }
     }
 }
