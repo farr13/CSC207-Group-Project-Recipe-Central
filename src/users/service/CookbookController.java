afacbd8f0daa51940a80a2fa6b51628e79c8e251
zachfu9669 <zachfu9669@gmail.com>
2023-11-27 18:30:14 -0500

    update the CookbookViewer-Controller

--- src/users/service/CookbookController.java
+++ src/users/service/CookbookController.java
@@ -1,4 +1,55 @@
-package users.service;
+package app;
+
+import backend.entity.Cookbook;
+import backend.entity.Recipe;
+import data_access.*;
+
+import java.io.IOException;
 
 public class CookbookController {
+
+    private final AddCookbookDAO addCookbookDAO;
+    private final DeleteCookbookDAO deleteCookbookDAO;
+    private final AddRecipeDAO addRecipeDAO;
+    private final DeleteRecipeDAO deleteRecipeDAO;
+
+    public CookbookController(AddCookbookDAO addCookbookDAO, DeleteCookbookDAO deleteCookbookDAO,
+                              AddRecipeDAO addRecipeDAO, DeleteRecipeDAO deleteRecipeDAO) {
+        this.addCookbookDAO = addCookbookDAO;
+        this.deleteCookbookDAO = deleteCookbookDAO;
+        this.addRecipeDAO = addRecipeDAO;
+        this.deleteRecipeDAO = deleteRecipeDAO;
+    }
+
+    public void addCookbook(Cookbook cookbook) {
+        try {
+            addCookbookDAO.addCookbook(cookbook);
+        } catch (IOException e) {
+            System.out.println("Error adding cookbook: " + e.getMessage());
+        }
+    }
+
+    public void deleteCookbook(Cookbook cookbook) {
+        try {
+            deleteCookbookDAO.deleteCookbook(cookbook);
+        } catch (IOException e) {
+            System.out.println("Error deleting cookbook: " + e.getMessage());
+        }
+    }
+
+    public void addRecipe(Recipe recipe, Cookbook cookbook) {
+        try {
+            addRecipeDAO.addRecipe(recipe, cookbook.getName());
+        } catch (IOException e) {
+            System.out.println("Error adding recipe: " + e.getMessage());
+        }
+    }
+
+    public void deleteRecipe(Recipe recipe, Cookbook cookbook) {
+        try {
+            deleteRecipeDAO.deleteRecipe(recipe, cookbook);
+        } catch (IOException e) {
+            System.out.println("Error deleting recipe: " + e.getMessage());
+        }
+    }
 }
--- src/users/service/CookbookViewer.java
+++ src/users/service/CookbookViewer.java
@@ -1,4 +1,25 @@
-package users.service;
+package app;
+
+import backend.entity.Cookbook;
+import data_access.AddCookbookDAO;
+import java.io.IOException;
 
 public class CookbookViewer {
+
+    private final AddCookbookDAO cookbookDAO;
+
+    public CookbookViewer(AddCookbookDAO cookbookDAO) {
+        this.cookbookDAO = cookbookDAO;
+    }
+
+    public void displayCookbook(String cookbookName) {
+        try {
+            Cookbook cookbook = cookbookDAO.getCookbook(cookbookName);
+            System.out.println("Cookbook: " + cookbook.getName());
+            cookbook.getRecipes().forEach(recipe ->
+                    System.out.println("Recipe: " + recipe.getName()));
+        } catch (IOException e) {
+            System.out.println("Error displaying cookbook: " + e.getMessage());
+        }
+    }
 }
