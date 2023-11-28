d509c160ee65513654e9a02e228052261986a60e
Zachary Fu <zacharyfu@Zacharys-MacBook-Pro.local>
2023-11-27 19:23:54 -0500

    Add DeleteCookbookDAO

---
+++ src/data_access/DeleteCookbookDAO.java
@@ -0,0 +1,14 @@
+package data_access;
+
+import app.NotImplementedException;
+import backend.entity.Cookbook;
+
+public class DeleteCookbookDAO { //implements DeleteCookbookInterface {
+
+    // private final File jsonFile;
+
+    public void DeleteCookbook(Cookbook cookbook) {
+        throw new NotImplementedException();
+    }
+
+}
