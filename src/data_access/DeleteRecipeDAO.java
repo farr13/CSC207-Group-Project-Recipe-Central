789e4f8dee55f01d33b40dbd10d980c58f67653d
Zachary Fu <zacharyfu@Zacharys-MacBook-Pro.local>
2023-11-27 19:24:58 -0500

    Add DeleteRecipeDAO

---
+++ src/data_access/DeleteRecipeDAO.java
@@ -0,0 +1,18 @@
+package data_access;
+
+import app.NotImplementedException;
+import backend.entity.Cookbook;
+import backend.entity.Recipe;
+
+public class DeleteRecipeDAO { // implements DeleteRecipeInterface {
+
+    // Again, I don't know what I'm doing here
+    // private final File jsonFile;
+    // private final Recipe rcp;
+    // private final Cookbook cb;
+
+    public void DeleteRecipe (Recipe recipe, Cookbook cookbook) {
+        throw new NotImplementedException();
+    }
+
+}
