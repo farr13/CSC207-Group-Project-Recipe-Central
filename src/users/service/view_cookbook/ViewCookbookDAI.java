9b6d307d76fd6f02c6ec5a33ad3eac480db99962
zachfu9669 <zachfu9669@gmail.com>
2023-11-28 18:51:01 -0500

    Complete ViewCookbook Usecase

--- src/users/service/view_cookbook/ViewCookbookDAI.java
+++ src/users/service/view_cookbook/ViewCookbookDAI.java
@@ -1,4 +1,21 @@
 package users.service.view_cookbook;
 
+import users.entity.Cookbook;
+
 public interface ViewCookbookDAI {
+    /**
+     * Retrieves a cookbook by its name from the JSON file.
+     *
+     * @param name The name of the cookbook.
+     * @return The cookbook with the specified name, or null if not found.
+     */
+    Cookbook getCookbookByName(String name);
+
+    /**
+     * Retrieves all cookbooks stored in the JSON file.
+     *
+     * @return An array or a list of all cookbooks.
+     */
+    Cookbook[] getAllCookbooks();
+
 }
