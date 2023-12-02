d4ea383751456d262b001f264d36b8ba089e568c
Aditya G <aditya.gautam@mail.utoronto.ca>
2023-12-02 18:12:33 -0500

    Checkout other branch, minor changes

--- src/view/recipe_objects/JRecipePanel.java
+++ src/view/recipe_objects/JRecipePanel.java
@@ -7,13 +7,11 @@ import javax.swing.*;
 public class JRecipePanel extends JPanel {
     public JRecipePanel(Triplet recipe){
         this.setBorder(BorderFactory.createTitledBorder(recipe.getName()));
-
         JLabel intructionsURL = new JLabel("Recipe Link: " + recipe.getLink());
         this.add(intructionsURL);
-
-        JList<String> ingredients = new JList<>(recipe.getList());
-        JScrollPane ingredientsPanel = new JScrollPane(ingredients);
-        ingredientsPanel.add(ingredients);
-        this.add(ingredientsPanel);
+        // JList<String> ingredients = new JList<>(recipe.getList());
+        // JScrollPane ingredientsPanel = new JScrollPane(ingredients);
+        // ingredientsPanel.add(ingredients);
+        // this.add(ingredientsPanel);
     }
 }
