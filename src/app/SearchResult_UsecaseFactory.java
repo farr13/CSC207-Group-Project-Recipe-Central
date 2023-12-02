47904c746b08ff67465cb5daf31e106bc64eefc6
farr13 <farrakhan.abdoulaye@mail.utoronto.ca>
2023-12-02 02:52:16 -0500

    Deleted Unrelated files

--- src/app/SearchResult_UsecaseFactory.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import view.usecase_views.SearchResultsView;
-import view.view_models.AddRecipeToCookbookViewmodel;
-import view.view_models.MainMenuViewModel;
-
-public class SearchResult_UsecaseFactory {
-    private SearchResult_UsecaseFactory() {}
-}
