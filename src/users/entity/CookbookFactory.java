56752b188292befdc0f78c5e29ad96ccba149dcd
Jalal <jalal.mustafayev@mail.utoronto.ca>
2023-12-02 13:22:40 -0500

    CookbookFactory Imrpovements

--- src/users/entity/CookbookFactory.java
+++ src/users/entity/CookbookFactory.java
@@ -1,27 +1,27 @@
 package users.entity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CookbookFactory {
 
-    public static Cookbook createCookbook(String name, ArrayList<Recipe> recipes) {
-        return new Cookbook(name, recipes);
+    public static Cookbook createCookbook(String name, List<Recipe> recipes) {
+        validateInput(name, recipes);
+        return new Cookbook(name, new ArrayList<>(recipes));
     }
 
     public static Cookbook createEmptyCookbook(String name) {
+        validateInput(name, null);
         return new Cookbook(name, new ArrayList<>());
     }
 
-    public static Cookbook createCookbookWithDefaultRecipes(String name) {
-        // Create a cookbook with some default recipes
-        ArrayList<Recipe> defaultRecipes = new ArrayList<>();
-        defaultRecipes.add(new Recipe("Default Pasta", "Instructions for default pasta"));
-        defaultRecipes.add(new Recipe("Default Cake", "Instructions for default pasta"));
-        return new Cookbook(name, defaultRecipes);
+    public static Cookbook createCookbookWithDefaultRecipes(String name, List<Recipe> defaultRecipes) {
+        validateInput(name, defaultRecipes);
+        return new Cookbook(name, new ArrayList<>(defaultRecipes));
     }
 
     public static Cookbook createRandomizedCookbook(String name, int numRecipes) {
-        // Create a cookbook with a specified number of randomly generated recipes
+        validateInput(name, null);
         ArrayList<Recipe> randomRecipes = generateRandomRecipes(numRecipes);
         return new Cookbook(name, randomRecipes);
     }
@@ -29,11 +29,21 @@ public class CookbookFactory {
     private static ArrayList<Recipe> generateRandomRecipes(int numRecipes) {
         ArrayList<Recipe> randomRecipes = new ArrayList<>();
         for (int i = 1; i <= numRecipes; i++) {
-            String recipeName = "Random Recipe " + i;
-            String instructions = "Instructions for " + recipeName;
-            randomRecipes.add(new Recipe(recipeName, instructions));
+            randomRecipes.add(createRandomRecipe("Random Recipe " + i));
         }
         return randomRecipes;
     }
 
+    private static Recipe createRandomRecipe(String name) {
+        return new Recipe(name, "Instructions for " + name);
+    }
+
+    private static void validateInput(String name, List<Recipe> recipes) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (recipes != null && recipes.contains(null)) {
+            throw new IllegalArgumentException("Recipe list cannot contain null elements");
+        }
+    }
 }
