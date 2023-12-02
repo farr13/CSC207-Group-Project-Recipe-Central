b219bfafb4632844903762b3670d18ea525d742b
Jalal <jalal.mustafayev@mail.utoronto.ca>
2023-12-01 19:48:18 -0500

    Added Validation

--- src/users/entity/IngredientFactory.java
+++ src/users/entity/IngredientFactory.java
@@ -1,2 +1,24 @@
-package users.entity;public class IngredientFactory {
+package users.entity;
+
+public class IngredientFactory {
+
+    public static Ingredient createIngredient(String name, String measureType, double quantity) {
+        validateIngredientParameters(name, measureType, quantity);
+        return new Ingredient(name, measureType, quantity);
+    }
+
+    public static Ingredient createUnitIngredient(String name, double quantity) {
+        validateIngredientParameters(name, "<unit>", quantity);
+        return new Ingredient(name, "<unit>", quantity);
+    }
+
+    public static Ingredient createDefaultIngredient(String name) {
+        return new Ingredient(name, "<unit>", 1.0);
+    }
+
+    private static void validateIngredientParameters(String name, String measureType, double quantity) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Ingredient name cannot be null or empty");
+        }
+    }
 }
