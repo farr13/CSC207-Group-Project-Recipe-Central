e707e8f10ac50412e8d4027d339accd878153787
Aditya G <aditya.gautam@mail.utoronto.ca>
2023-12-07 22:26:24 -0500

    Commented unused code & added javadocs for all adapters in app/backed/

--- src/backend/adapters/RecipesToTriplets.java
+++ src/backend/adapters/RecipesToTriplets.java
@@ -6,11 +6,14 @@ import view.recipe_objects.Triplet;
 
 import java.util.ArrayList;
 
+/** A class for converting a list of Recipe Objects to a list of Triplet adapters. */
 public class RecipesToTriplets {
-    private RecipesToTriplets(){}
+    /** Converts a list of Recipe Objects to a list of Triplet adapters.
+     * @param recipes type Recipe[]
+     * @return Triplet[] */
     public static Triplet[] convert(Recipe[] recipes){
-        ArrayList<Triplet> triplets = new ArrayList<Triplet>();
-        for (Recipe recipe: recipes){
+        ArrayList<Triplet> triplets = new ArrayList<>();
+        for (Recipe recipe: recipes) {
             String name = recipe.getName();
             String link = recipe.getInstructions();
             ArrayList<String> arrayList = new ArrayList<String>();
--- src/backend/adapters/TripletsToRecipeBlocks.java
+++ src/backend/adapters/TripletsToRecipeBlocks.java
@@ -4,11 +4,14 @@ import view.recipe_objects.Triplet;
 
 import java.util.ArrayList;
 
+/** A class for converting a list of strings representing recipes to a list of Triplet adapters. */
 public class TripletsToRecipeBlocks {
-    private TripletsToRecipeBlocks(){}
+    /** Converts a list of strings representing recipes to a list of Triplet adapters.
+     * @param recipes String[]
+     * @return Sting[] */
     public static String[] convert(Triplet[] recipes){
-        ArrayList<String> recipeBlocks = new ArrayList<String>();
-        for (Triplet recipe: recipes){
+        ArrayList<String> recipeBlocks = new ArrayList<>();
+        for (Triplet recipe: recipes) {
             StringBuilder temp;
             String[] ingredients = recipe.getList();
             temp = new StringBuilder(("<html>Recipe:<html>  " + "<html>_" + recipe.getName() + "_<html>" + "<br>Instructions: _"
@@ -19,7 +22,6 @@ public class TripletsToRecipeBlocks {
             temp.append("_<br> <br>");
             recipeBlocks.add(temp.toString());
         }
-
         return recipeBlocks.toArray(new String[0]);
     }
 }
